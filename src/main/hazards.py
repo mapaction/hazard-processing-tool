01807bae8f22e0fa268846ce88c155bb2f6fb7e3
ediakatos <ediakatos@mapaction.org>
2025-04-01 13:26:35 +0100

    fixed paths

--- src/main/hazards.py
+++ src/main/hazards.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from typing import List
 from typing import Dict
-from src.utils.constants import ( HAZARD_INPUT_PATH )
+from src.utils.constants import ( HAZARD_INPUT_PATH, S3_BUCKET )
 from src.utils.utils import ( 
     compute_hazard_population_exposure,
     compute_binary_zonal_stat,
@@ -34,7 +34,7 @@ def process_cyclone(admin_df: gpd.GeoDataFrame) -> pd.DataFrame:
     Process cyclone data
     """
     df = admin_df.drop(columns='geometry').copy()
-    hazard_raster = xr.open_dataarray(HAZARD_INPUT_PATH['cyclone'])
+    hazard_raster = xr.open_dataarray(f"/vsis3/{S3_BUCKET}/{HAZARD_INPUT_PATH['cyclone']}")
     hazard_data = hazard_raster[0].values
     df['max_speed'] = compute_zonal_stat(hazard_data, hazard_raster.rio.transform(), admin_df, agg='max')
     return df
@@ -44,7 +44,7 @@ def coastal_erosion(admin_df: gpd.GeoDataFrame) -> pd.DataFrame:
     Process coastal erosion data"
     """
     adm_col = next(col for col in ['adm2_src', 'adm1_src', 'adm0_src'] if col in admin_df.columns)
-    hazard_df = gpd.read_file(HAZARD_INPUT_PATH['coastal_erosion'])
+    hazard_df = gpd.read_file(f"/vsis3/{S3_BUCKET}/{HAZARD_INPUT_PATH['coastal_erosion']}")
     admin_df.geometry = admin_df.geometry.buffer(0.01)
     hazard_df = hazard_df[['rate_time', 'geometry']]
     merge_df = gpd.sjoin(admin_df, hazard_df)
